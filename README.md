1c36b9005068cdc15c881700f673e8552a55f08a
wuzhiqiang <755960141@qq.com>
2024-01-14 01:19:03 +0800

    docs: README.md

---
+++ README.md
@@ -0,0 +1,27 @@
+<p align="center">
+    <img width="130px" height="130px" src='https://user-images.githubusercontent.com/799578/50462941-8075fe80-09c3-11e9-89e7-af0cb7991406.png' alt="">
+</p>
+
+![](https://img.shields.io/jetbrains/plugin/v/wzq.codelf.plugin) ![](https://img.shields.io/jetbrains/plugin/r/stars/wzq.codelf.plugin) ![](https://img.shields.io/jetbrains/plugin/d/wzq.codelf.plugin) ![](https://img.shields.io/github/license/Zhiqiang-Wu/idea-codelf-plugin)
+
+## Language
+
+[![typescript](https://skillicons.dev/icons?i=typescript)](https://www.typescriptlang.org)
+
+## Framework
+
+[![react](https://skillicons.dev/icons?i=react)](https://react.dev)
+
+## Related projects
+
+-   [codelf](https://github.com/unbug/codelf)
+-   [searchcode](https://searchcode.com/)
+-   [idea-codelf-plugin](https://github.com/Zhiqiang-Wu/idea-codelf-plugin)
+
+## Semver
+
+Plugin is following [Semantic Versioning 2.0.0](https://semver.org/)
+
+## License
+
+[MIT](./LICENSE)
--- src/pages/home/view/index.tsx
+++ src/pages/home/view/index.tsx
@@ -1,5 +1,69 @@
-const HomeView = () => {
-    return <div>home</div>;
+import { useCreation, useMemoizedFn } from 'ahooks';
+import { Button, Dropdown, Spin } from 'antd';
+import { TagCloud } from 'react-tagcloud';
+
+const HomeView = ({
+    tags,
+    spinning,
+}: {
+    tags: Array<any>;
+    spinning: boolean;
+}) => {
+    const colors = useCreation(() => {
+        return ['red', 'orange', 'olive', 'green', 'teal', 'blue', 'purple'];
+    }, []);
+
+    const randomColor = useMemoizedFn(() => {
+        const index = Math.floor(Math.random() * colors.length);
+        return colors[index];
+    });
+
+    const getItems = useMemoizedFn((tag) => [
+        {
+            key: '1',
+            label: (
+                <a target="_blank" href={tag.repo} rel="noreferrer">
+                    Repo
+                </a>
+            ),
+        },
+        {
+            key: '2',
+            disabled: true,
+            label: <span>[{tag.language}]</span>,
+        },
+    ]);
+
+    const renderer = useMemoizedFn((tag) => {
+        return (
+            <Dropdown arrow key={tag.value} menu={{ items: getItems(tag) }}>
+                <Button
+                    shape="round"
+                    style={{
+                        fontWeight: 'bold',
+                        margin: 2,
+                        backgroundColor: randomColor(),
+                        border: 'none',
+                        color: 'white',
+                    }}
+                >
+                    {tag.value}
+                </Button>
+            </Dropdown>
+        );
+    });
+
+    return (
+        <>
+            <TagCloud
+                tags={tags}
+                maxSize={50}
+                minSize={20}
+                renderer={renderer}
+            />
+            <Spin size="large" fullscreen spinning={spinning} />
+        </>
+    );
 };
 
 export default HomeView;
